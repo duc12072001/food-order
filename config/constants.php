84f891f7d305434c06a808ee54100a6fd5c115e0
duc12072001 <71589748+duc12072001@users.noreply.github.com>
2021-04-14 11:34:57 +0700

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,4 @@
 <?php 
-    //Start Session
-    session_start();
 
 
     //Create Constants to Store Non Repeating Values
@@ -9,7 +7,4 @@
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '123456789');
     define('DB_NAME', 'food-order');
-    
-    $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //Database Connection
-    $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error()); //SElecting Database
 ?>
